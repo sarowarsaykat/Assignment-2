985757364d397ce92bbec5b33e5dc3bcd9d4ee60
Md Sarowar Jaman Saykat <147086503+sarowarsaykat@users.noreply.github.com>
2024-08-23 23:11:56 +0600

    Add files via upload

---
+++ Assignment-2/oop.php
@@ -0,0 +1,66 @@
+<?php
+
+class Book {
+    private $title;
+    private $availableCopies;
+    public function __construct($title, $availableCopies) {
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+    }
+    public function getTitle() {
+        return $this->title;
+    }
+    public function getAvailableCopies() {
+        return $this->availableCopies;
+    }
+    public function borrowBook() {
+        if ($this->availableCopies > 0) {
+            $this->availableCopies--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function returnBook() {
+        $this->availableCopies++;
+    }
+}
+
+class Member {
+    private $name;
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function borrowBook($book) {
+        if ($book->borrowBook()) {
+            echo $this->name . " has borrowed \"" . $book->getTitle() . "\".<br>";
+        } else {
+            echo "Sorry, " . $book->getTitle() . " is not available.<br>";
+        }
+    }
+    public function returnBook($book) {
+        $book->returnBook();
+        echo $this->name . " has returned \"" . $book->getTitle() . "\".<br>";
+    }
+}
+
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
+$member1 = new Member("John Doe");
+$member2 = new Member("Jane Smith");
+
+$member1->borrowBook($book1);
+$member2->borrowBook($book2); 
+echo "Available copies of \"" . $book1->getTitle() . "\": " . $book1->getAvailableCopies() . "<br>";
+echo "Available copies of \"" . $book2->getTitle() . "\": " . $book2->getAvailableCopies() . "<br>";
+
+$member1->returnBook($book1);
+$member2->returnBook($book2); 
+echo "Available copies of \"" . $book1->getTitle() . "\": " . $book1->getAvailableCopies() . "<br>";
+echo "Available copies of \"" . $book2->getTitle() . "\": " . $book2->getAvailableCopies() . "<br>";
+
+?>
